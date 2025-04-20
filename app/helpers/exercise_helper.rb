0b8a9997ea9a3dbfcc67497d2721f0e797f378f5
Stanko K.R. <stanko@stanko.io>
2025-04-20 08:12:00 +0200

    Fix flashing during animation transitions on Firefox

--- app/helpers/exercise_helper.rb
+++ app/helpers/exercise_helper.rb
@@ -13,8 +13,8 @@ module ExerciseHelper
 
         content_tag(:div, "", class: "absolute inset-0 z-40 flex justify-center items-center cursor-pointer text-black text-xl", data: { action: "click->exercise#toggle", exercise_target: "overlay" }) +
         animated_circle(symbol: circle_id, class: "hidden") +
-        animated_circle(use: circle_id, class: "absolute inset-0 z-30 w-full select-none transition-all duration-300", stroke: "black", stroke_width: 2, name: "exercise-circle-min", data: { exercise_target: "minCircle" }) +
-        animated_circle(use: circle_id, class: "absolute inset-0 z-20 w-full select-none transition-all", data: { exercise_target: "progressCircle" }) +
+        animated_circle(use: circle_id, class: "absolute inset-0 z-30 w-full select-none", stroke: "black", stroke_width: 2, name: "exercise-circle-min", data: { exercise_target: "minCircle" }) +
+        animated_circle(use: circle_id, class: "absolute inset-0 z-20 w-full select-none", data: { exercise_target: "progressCircle" }) +
         animated_circle(use: circle_id, class: "absolute inset-0 z-10 w-full select-none opacity-10", name: "exercise-circle-max", data: { exercise_target: "maxCircle" })
       end +
       content_tag(:div, "", class: "flex justify-center select-none min-h-6", data: { exercise_target: "timer" })
